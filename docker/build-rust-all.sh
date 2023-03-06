5d184ff6e06bdb0b0f4c43963d7a6192086451ca
Larry Liu <larry@aptoslabs.com>
2023-03-06 10:28:56 -0800

    Add the binary.

--- docker/build-rust-all.sh
+++ docker/build-rust-all.sh
@@ -51,6 +51,7 @@ BINS=(
     aptos-indexer-grpc-cache-worker
     aptos-indexer-grpc-file-store
     aptos-indexer-grpc-data-service
+    aptos-indexer-grpc-parser
     aptos-fn-check-client
     aptos-db-tool
     aptos-db-bootstrapper
